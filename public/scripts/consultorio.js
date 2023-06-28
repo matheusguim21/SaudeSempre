2ef8b6df23e5cf1179432fd746b6d46a157efc4d
Matheus Guimarães <matheusguim13@gmail.com>
2023-06-28 00:54:26 -0300

    Consultorios INSERT E DELETE OK

--- public/scripts/consultorio.js
+++ public/scripts/consultorio.js
@@ -1,7 +1,7 @@
 const mainScreen = document.getElementById('mainScreen');
 const addButton = document.getElementById('addButton');
 const addScreen = document.getElementById('addScreen');
-const closeAddScreenButton = document.querySelector('.close-button');
+const closeAddScreenButton = addScreen.querySelector('.close-button');
 
 addButton.addEventListener('click', () => {
   addScreen.style.display = 'block';
@@ -42,58 +42,113 @@ cep.addEventListener("blur", (e) => {
 });
 
 // consultorio.js
-document.addEventListener('DOMContentLoaded', () => {
-  const modal = document.getElementById('conclusaoModal');
-  const closeButton = document.querySelector('#conclusaoModal .close-button');
-  const fecharModalButton = document.getElementById('fecharModal');
 
-  closeButton.addEventListener('click', () => {
-    modal.style.display = 'none';
-  });
+document.addEventListener('DOMContentLoaded', function() {
+  const addButton = document.getElementById('addButton');
+  const saveButton = document.getElementById('saveButton');
+  const editButtons = document.querySelectorAll('.btn-edit');
+  const removeButtons = document.querySelectorAll('.btn-remove');
 
-  fecharModalButton.addEventListener('click', () => {
-    modal.style.display = 'none';
-  });
-  })
-  
-// Captura o evento de clique do botão "Remover"
-document.addEventListener('click', function(event) {
-  if (event.target.classList.contains('btn-remove')) {
-    // Obtém o ID do consultório a ser removido
-    const consultorioId = event.target.dataset.id;
-    const enderecoId = event.target.dataset.endereco;
-
-    // Verifica se o ID do consultório e do endereço estão definidos
-    if (consultorioId ) {
-
-      console.log(consultorioId)
-      console.log(enderecoId)
-
-      // Cria um objeto com os dados a serem enviados no corpo da requisição
-      const 
-      dados = {
-      consultorioId: consultorioId,
-      enderecoId: enderecoId
-      };
-        // Envia uma solicitação de exclusão para o servidor
-  fetch(`/consultorio/${consultorioId}/${enderecoId}`, {
-    method: 'DELETE',
-    
-  })
-    .then(response => {
-      // Verifica se a solicitação foi bem-sucedida
-      if (response.ok) {
-        // Atualiza a página ou realiza qualquer outra ação necessária
-        window.location.reload();
-      } else {
-        console.error('Erro ao excluir o consultório');
-      }
-    })
-    .catch(error => {
-      console.error('Erro ao enviar a solicitação de exclusão', error);
+  if (addButton) {
+    addButton.addEventListener('click', function() {
+      document.getElementById('addScreen').style.display = 'block';
     });
-  } else {
-    console.error('ID do consultório não está definido');
   }
-}
+
+  if (saveButton) {
+    saveButton.addEventListener('click', function() {
+      const nome = document.getElementById('nome').value;
+      const idEndereco = document.getElementById('idEndereco').value;
+      const cep = document.getElementById('cep').value;
+      const rua = document.getElementById('rua').value;
+      const numero = document.getElementById('numero').value;
+      const bairro = document.getElementById('bairro').value;
+      const cidade = document.getElementById('cidade').value;
+      const estado = document.getElementById('estado').value;
+
+      const data = {
+        nome: nome,
+        idEndereco: idEndereco,
+        cep: cep,
+        rua: rua,
+        numero: numero,
+        bairro: bairro,
+        cidade: cidade,
+        estado: estado
+      };
+
+      fetch('/consultorio', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+        .then(response => {
+          if (response.ok) {
+            document.getElementById('conclusaoModal').style.display = 'block';
+          } else {
+            console.error('Erro ao salvar o consultório');
+          }
+        })
+        .catch(error => {
+          console.error('Erro ao enviar a solicitação de salvamento', error);
+        });
+    });
+  }
+
+  if (editButtons) {
+    editButtons.forEach(function(button) {
+      button.addEventListener('click', function(event) {
+        const consultorioId = event.target.dataset.id;
+        const nome = event.target.dataset.nome;
+        const idEndereco = event.target.dataset.idEndereco;
+        const cep = event.target.dataset.cep;
+        const rua = event.target.dataset.rua;
+        const numero = event.target.dataset.numero;
+        const bairro = event.target.dataset.bairro;
+        const cidade = event.target.dataset.cidade;
+        const estado = event.target.dataset.estado;
+
+        document.getElementById('editModal').style.display = 'block';
+        console.log('Chegou aqui')
+        document.getElementById('consultorioIdInput').value = consultorioId;
+        document.getElementById('editNomeInput').value = nome;
+        document.getElementById('editIdEnderecoInput').value = idEndereco;
+        document.getElementById('editCepInput').value = cep;
+        document.getElementById('editRuaInput').value = rua;
+        document.getElementById('editNumeroInput').value = numero;
+        document.getElementById('editBairroInput').value = bairro;
+        document.getElementById('editCidadeInput').value = cidade;
+        document.getElementById('editEstadoInput').value = estado;
+      });
+    });
+  }
+
+  if (removeButtons) {
+    removeButtons.forEach(function(button) {
+      button.addEventListener('click', function(event) {
+        const consultorioId = event.target.dataset.id;
+        const enderecoId = event.target.dataset.endereco;
+
+        if (consultorioId) {
+          fetch(`/consultorio/${consultorioId}/${enderecoId}`, {
+            method: 'DELETE',
+          })
+            .then(response => {
+              if (response.ok) {
+                window.location.reload();
+              } else {
+                console.error('Erro ao excluir o consultório');
+              }
+            })
+            .catch(error => {
+              console.error('Erro ao enviar a solicitação de exclusão', error);
+            });
+        } else {
+          console.error('ID do consultório não está definido');
+        }
+      });
+    });
+  }
 });
